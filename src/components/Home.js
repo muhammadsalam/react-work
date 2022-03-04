1f826c19b801f9575d606450f37e247eedbebeb3
Magomedsalam Musaev <muhammadsalam@list.ru>
2022-03-05 00:43:46 +0300

    #13 Functions as Props

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,11 +8,14 @@ function Home() {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All blogs!" />
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs" />
-            {/* переиспользовал массив так, чтобы показывались только блоги от автора Марио */}
+            <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete}/>
         </div>
     );
 }
