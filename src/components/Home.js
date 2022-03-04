f2c4d7bac9185e5d174086c4cb4a17ef5a271271
Magomedsalam Musaev <muhammadsalam@list.ru>
2022-03-04 09:51:36 +0300

    #8 Using State (useState hook) в комментариях указал как оно работает

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,29 @@
+import { useState } from 'react';
+
 function Home() {
+    
+    // Что такое useState ?
+    // Это возможность ререндеринга компонента
+    // например, если менять значение переменной, то в компоненте оно не будет меняться, а просто останется старым. Для этого используют ререндеринг компонента, чтобы можно было видеть новые значения.
+
+    // ps. Angular позволяет делать это автоматически, без постороннего.
+
+    const [name, setName] = useState('mario');
+
+    // const [1, 2] - вместо 1 - название переменной, а вместо 2 - название функции, которая позволяет изменять эту переменную
+    
+    const [age, setAge] = useState(18);
 
     const handleClick = () => {
-        console.log("hello ninjas");
-    };
-
-    const handleClickAgain = (name, e) => {
-        console.log('Hello ' + name, e.target);
+        setName('Luigi');
+        setAge('30');
     };
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => handleClickAgain('Mario', e)}>Click me again</button>
         </div>
     );
 }
