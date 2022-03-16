da25eea25e1fa485968cae2b5156f52212aabbf0
Magomedsalam Musaev <muhammadsalam@list.ru>
2022-03-16 13:12:14 +0300

    Fetching Data with useEffect fetch запрос

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,4 @@
-export default function BlogList ({blogs, title, handleDelete}) {
+export default function BlogList ({blogs, title}) {
     return ( 
         <div className="blog-list">
             <h2>{ title }</h2>
@@ -6,7 +6,6 @@ export default function BlogList ({blogs, title, handleDelete}) {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
                     <p>Written byt { blog.author }</p>
-                    <button onClick={() => handleDelete(blog.id)}>delete blog</button>
                 </div>  
             ))}
         </div>
